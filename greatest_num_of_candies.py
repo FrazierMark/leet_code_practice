254bba680cc84c1f879b5aed6168c5ff5585707e
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-05-26 22:48:27 -0500

    Leetcode for May 27th, 2021.

---
+++ greatest_num_of_candies.py
@@ -0,0 +1,34 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+    	
+
+
+
+#Steps
+# find largest int in the array
+# set a variable === to that
+
+# if extra candies PLUS the candies[i]
+# is greater that extraCandies:...
+
+# then we want to output true
+# else output false
+
+
+
+
+# Example
+# Input: candies = [2,3,5,1,3], extraCandies = 3
+# Output: [true,true,true,false,true] 
+
+
+
+# Given the array candies and the integer extraCandies,
+# where candies[i] represents the number of candies
+# that the ith kid has.
+
+# For each kid check if there is a way to distribute
+# extraCandies among the kids such that he or she can
+# have the greatest number of candies among them.
+# Notice that multiple kids can have the greatest
+
--- move_zeroes.py
+++ move_zeroes.py
@@ -13,8 +13,20 @@ class Solution:
 
 
 
+# Smart solution found online- swapping elements
 
-
+class Solution:
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        pos = 0
+        for i in range(len(nums)):
+            el = nums[i]
+            if el != 0:
+                nums[pos], nums[i] = nums[i], nums[pos]
+                pos += 1
 
 
 # Example 1:
