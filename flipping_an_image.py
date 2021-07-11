a8b0754cd1971bc0e29b4443d39991f41633ca51
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-07-10 20:08:45 -0700

    Leetcode for July 10, 2021. Havent been productive. Just moved to Berkeley. Moving sucks.

---
+++ flipping_an_image.py
@@ -0,0 +1,49 @@
+class Solution:
+	def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+		for row in image:
+			row.reverse()
+		for row in image:
+			for element in range(len(row)):
+				if row[element] == 1:
+					row[element] = 0
+				else:
+					row[element] = 1
+		return image
+
+
+
+# Steps
+# - step into matrix/ row and reverse each row
+
+# - step into row, then step into element and if 0 make 1, if 1 make zero
+
+
+
+# 832. Flipping an Image
+
+
+# Given an n x n binary matrix image, flip the image horizontally,
+# then invert it, and return the resulting image.
+
+# To flip an image horizontally means that each row
+# of the image is reversed.
+# For example, flipping [1,1,0] horizontally results in [0,1,1].
+
+
+# To invert an image means that each 0 is replaced by 1,
+# and each 1 is replaced by 0.
+# For example, inverting [0,1,1] results in [1,0,0].
+ 
+
+# Example 1:
+
+# Input: image = [[1,1,0],[1,0,1],[0,0,0]]
+# Output: [[1,0,0],[0,1,0],[1,1,1]]
+# Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+# Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+# Example 2:
+
+# Input: image = [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+# Output: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+# Explanation: First reverse each row: [[0,0,1,1],[1,0,0,1],[1,1,1,0],[0,1,0,1]].
+# Then invert the image: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
